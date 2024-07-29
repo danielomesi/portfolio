07aaa3e88236647ad658d9724da248ac1453290c
Daniel Omesi <omesidaniel@gmail.com>
2024-07-29 14:07:48 +0300

    Small changes

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function openModal() {
+  document.getElementById('videoModal').style.display = 'flex';
+}
+
+// Function to close the modal
+function closeModal() {
+  const modal = document.getElementById('videoModal');
+  modal.style.display = 'none';
+  const video = document.getElementById('demoVideo');
+  video.pause();
+  video.currentTime = 0;
+}
+
+// Close modal when clicking outside of the modal content
+window.onclick = function(event) {
+  const modal = document.getElementById('videoModal');
+  if (event.target == modal) {
+    closeModal();
+  }
+}
