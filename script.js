84178b5e14fbc4b7350e413e8fa9cd599b970575
Daniel Omesi <omesidaniel@gmail.com>
2025-01-06 21:58:26 +0200

    Added leetcode solutions video sections

--- script.js
+++ script.js
@@ -24,3 +24,39 @@ window.onclick = function(event) {
     closeModal();
   }
 }
+
+// List of video sources and titles
+const videos = [
+  { src: 'assets/Two Sum.mkv', title: 'Two Sum' },
+  { src: 'assets/Three Sum Closest.mkv', title: 'Three Sum Closest' },
+  { src: 'assets/Container With Most Water.mkv', title: 'Container With Most Water' },
+  { src: 'assets/Zigzag Conversion.mkv', title: 'Zigzag Conversion' }
+];
+
+let currentIndex = 0;
+
+function changeVideo(direction) {
+  if (direction === 'next') {
+      currentIndex = (currentIndex + 1) % videos.length;
+  } else if (direction === 'prev') {
+      currentIndex = (currentIndex - 1 + videos.length) % videos.length;
+  }
+
+   setVideo()
+}
+
+function setVideo() {
+    // Set the first video explicitly
+    const videoPlayer = document.getElementById('videoPlayer');
+    const videoTitle = document.getElementById('videoTitle');
+    
+    videoPlayer.src = videos[currentIndex].src;
+    videoTitle.textContent = videos[currentIndex].title;
+  
+    // Load and play the first video
+    videoPlayer.load();
+}
+
+window.onload = function() {
+    setVideo()
+};
